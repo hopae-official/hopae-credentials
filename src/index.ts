041de6261c4125d49e65dcd125fd947b0725ff47
Mike Xu <mxu11290@gmail.com>
2019-07-01 09:50:03 -0400

    do not rename imports in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
-import Creds from './Credentials'
-import { SimpleSigner as S } from 'did-jwt'
-import { ContractFactory as CF } from './Contract'
+import Credentials from './Credentials'
+import { SimpleSigner } from 'did-jwt'
+import { ContractFactory } from './Contract'
 
-export const Credentials = Creds
-export const SimpleSigner = S
-export const ContractFactory = CF
+export { Credentials, SimpleSigner, ContractFactory }
 export default { Credentials, SimpleSigner, ContractFactory }
